1007eca6a71055a5c3f4288c4f85784d36ae60cd
ricardoparizotto <ricardo.dparizotto@gmail.com>
2023-12-09 23:55:23 +0000

    changed grammar

--- interface.py
+++ interface.py
@@ -13,7 +13,11 @@ reserved = {
    'create':        'CREATE',
    'update':        'UPDATE',
    'intent':        'INTENT',
-   'delete':        'DELETE'
+   'delete':        'DELETE',
+   'functionality': 'FUNCTIONALITY',
+   'high':          'HIGH',
+   'low':           'LOW',
+   'priority':      'PRIORITY'
 }
 
 # List of token names.   This is always required
@@ -28,7 +32,8 @@ tokens = [
    'NAME',
    'DDOT',
    'NUMBER',
-   'ID'
+   'ID',
+   'VALUE'
 ] + list(reserved.values())
 
 #add reserved words as tokens
@@ -36,8 +41,6 @@ def t_ID(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
     t.type = reserved.get(t.value,'ID')    # Check for reserved words
     return t
-
-print(tokens)
 
 # Regular expression rules for simple tokens
 t_LCOLCH   = r'\['
@@ -50,27 +53,53 @@ t_NAME    = r'@[a-z]+'
 t_COMMA   = r','
 t_DDOT    = r':'
 t_NUMBER  = r'[0-9]+'
+t_VALUE   = r'&[a-zA-Z_0-9][a-zA-Z_0-9]*'
 
 t_ignore = ' \t'
-#lexer = lex.lex()
+lexer = lex.lex()
 
 def p_intent(t):
     #TODO: intent name and functionality
-    'intent : operation INTENT NAME predicates'
+    'intent : operation INTENT NAME LBRACKET predicates RBRACKET'
     operator = t[1]
     intent_name = t[3].strip()
-    predicate = t[4]
+    predicate = t[5]
     print(f"Operation: {operator}, Intent Name: {intent_name}, Predicate:{predicate}")
 
 def p_predicates(t):
-    '''predicates : predicate COMMA predicate
-                    | predicate'''
+    '''predicates : functionality COMMA requirement COMMA priority'''
+    t[0] = {'functionality': t[1], 'requirements' : t[3] , 'priority' : t[5]}
+
+def p_functionality(t):
+    'functionality : FUNCTIONALITY DDOT NAME LCOLCH inputs RCOLCH'
+    t[0] = t[3]
+
+def p_inputs(t):
+    '''inputs : input COMMA inputs
+                | input
+                | empty '''
+
+def p_empty(p):
+    'empty :'
+    pass
+
+def p_input(t):
+    'input : NAME DDOT VALUE'
+
+def p_priority(t):
+    '''priority : PRIORITY HIGH
+                  | PRIORITY LOW '''
+    t[0] = t[2]
+
+def p_requirements(t):
+    '''requirements : requirement COMMA requirements
+                    | requirement'''
     t[0] = t[1]
     #TODO: if for comma and first rule
 
-def p_predicate(t):
-    'predicate : requirement'
-    t[0] = t[1]
+#def p_predicate(t):
+#    'predicate : requirement'
+#    t[0] = t[1]
 
 def p_requirement(t):
     '''requirement : CONS consistency
@@ -80,16 +109,11 @@ def p_requirement(t):
 def p_consistency(t):
     '''consistency : STRONG
                      | WEAK'''
-    global var_consistency
-    var_consistency = str(t[1])
     t[0] = t[1]
 
 def p_availability(t):
     'availability : NUMBER'
     t[0] = int(t[1])
-
-    global var_availability
-    var_availability = int(t[1])
 
 def p_operation(t):
     '''operation : CREATE
@@ -98,14 +122,16 @@ def p_operation(t):
     t[0] = t[1]
 
 def p_error(t):
-    print(f"Syntax error at {t.value[0]}")
+    print(f"Syntax error at {t.value}")
     t.lexer.skip(1)
 # Build the lexer
-lexer = lex.lex()
+#lexer = lex.lex()
 # Build the parser
 parser = yacc.yacc()
 
-s = "create intent @intentname consistency strong"
+gpt = "create intent @machine_learning_training { functionality : @CNN_model, availabity: 1, PRIORITY: HIGH }"
+
+s = "create intent @intentname { functionality : @teste [ @size : &3 ], consistency strong, priority high }"
 
 
-result = parser.parse(s)
+result = parser.parse(gpt)
